fd6c50a4896ce7c5002a12797af1faf25925d223
aglaeDev <a.gourdet.dev@gmail.com>
2023-07-10 11:57:17 +0200

    exo 01 done

---
+++ lib/01_temperature.rb
@@ -0,0 +1,16 @@
+def ftoc (fahrenheit)
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
+end
+
+def ctof (celsius)
+    fahrenheit = ( celsius.to_f * 9 / 5) + 32 
+    if fahrenheit == fahrenheit.to_i
+        return fahrenheit.to_i
+    else
+        return fahrenheit.to_f
+    end
+end
+
+
+ctof(0)
