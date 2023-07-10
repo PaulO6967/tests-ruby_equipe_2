73d8972486b4e0e736195804310db4b3462b1ab6
PaulO6967 <p.osswald67@gmail.com>
2023-07-10 20:23:31 +0200

    final correction exo2 done

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -7,9 +7,27 @@ def subtract (number_1,number_2)
 end
 
 def sum (arrey)
- return arrey.sum
+  return arrey.sum
 end
 
 def multiply (number_1,number_2)
   return number_1 * number_2
+end
+
+def power (number_1, number_2)
+  return number_1 ** number_2
+end
+
+def factorial (number)
+  fact = 1
+  if (number ==0)
+    return 1
+  else
+    i=1
+    while(i<=number)
+      fact=fact*i
+      i+=1
+    end
+  end
+    return fact
 end
