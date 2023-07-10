3c1d7539ece0ee1ea6b76de29c62877077673c63
PaulO6967 <p.osswald67@gmail.com>
2023-07-10 13:13:36 +0200

    exo 02_calculator.rb done

---
+++ lib/02_calculator.rb
@@ -0,0 +1,15 @@
+def add (number_1,number_2)
+  return number_1 + number_2
+end
+
+def subtract (number_1,number_2)
+  return number_1 - number_2
+end
+
+def sum (arrey)
+ return arrey.sum
+end
+
+def multiply (number_1,number_2)
+  return number_1 * number_2
+end
